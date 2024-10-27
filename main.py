00c194b2e8bb1c7088ff788cb80fedd7b17f7423
Luke Canada <canadaluke888@gmail.com>
2024-10-27 11:57:09 -0400

    Integrated rich objects to make output look cleaner. Error handling added to settings and API call.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from settings.settings import Settings
 from rich.panel import Panel
 from rich.table import Table
 from rich.console import Console
+from openai import BadRequestError
 
 console = Console()
 extract = Extract()
@@ -20,15 +21,30 @@ def main():
         command = console.input("[red]Enter a command:[/red] ").lower()
 
         if command == 'summarize':
+
+            if not settings.check_for_api_key():
+                console.print(Panel("[bold][red]API Key not set.[/red][/bold]", title='Information', title_align='center', border_style='white'))
+                continue
+
             video_path = console.input("[orange]Enter path to video:[/orange]")
 
-            # Extract and summarize content
-            video_frames = extract.extract_highlight_frames(video_path)
-            video_audio_path = extract.extract_audio(video_path)
-            audio_transcript = summarize.get_audio_transcript(video_audio_path)
-            video_description = summarize.describe_video_from_frames(video_frames)
-            audio_transcript_summary = summarize.summarize_transcript(audio_transcript)
-            overall_summary = summarize.write_overall_summary(video_description, audio_transcript_summary)
+            if video_path.lower() == 'cancel' or video_path == " ":
+                console.print(Panel("[bold][red]Cancelling Summarization...[/red][/bold]", title="Information", title_align='center', border_style='white'))
+                continue
+
+
+            try:
+                # Extract and summarize content
+                video_frames = extract.extract_highlight_frames(video_path)
+                video_audio_path = extract.extract_audio(video_path)
+                audio_transcript = summarize.get_audio_transcript(video_audio_path)
+                video_description = summarize.describe_video_from_frames(video_frames)
+                audio_transcript_summary = summarize.summarize_transcript(audio_transcript)
+                overall_summary = summarize.write_overall_summary(video_description, audio_transcript_summary)
+
+            except BadRequestError as e:
+                console.print(Panel(f"[bold][red]Error while generating summary:[/red] [orange]\n\n{e}[/orange][/bold]", title="Error", title_align='center', border_style='white'))
+                continue
 
             audio_transcript_panel = Panel(
                 audio_transcript,
@@ -65,7 +81,7 @@ def main():
             break
 
         elif command not in command_list:
-            console.print("[red]\nInvalid command![/red]")
+            console.print(Panel("[bold][red]Invalid Command![/red][/bold]", title="Error", title_align='center', border_style='white'))
             continue
 
 if __name__ == '__main__':
--- settings/settings.py
+++ settings/settings.py
@@ -1,51 +1,83 @@
 import json
 import os
-
+from rich.panel import Panel
+from rich.console import Console
+from rich.table import Table
 
 class Settings:
     def __init__(self):
+
+        self.console = Console()
 
         self.settings_file_path = os.path.join(os.path.dirname(__file__), "settings.json")
         self.current_settings = self.load_settings()
 
     def handle_settings(self):
+        current_settings_table = Table(title="Current Settings", show_lines=True, border_style='white')
+        current_settings_table.add_column("Setting", style='cyan', no_wrap=True)
+        current_settings_table.add_column("Value", style="magenta")
 
-        print(f"\nCurrent Settings: \n{self.current_settings}")
+        for setting, value in self.current_settings.items():
+            current_settings_table.add_row(str(setting), str(value))
+
+        self.console.print(current_settings_table)
 
         while True:
-
-            setting = input("\nEnter a setting: ")
+            setting = self.console.input("\n[green]Enter a setting:[/green]")
 
             if setting == 'openai_api_key':
-                api_key = input("\nEnter OpenAI API Key: ")
+                api_key = self.console.input("\n[green]Enter API Key:[/green]")
 
-                if api_key.lower() is "cancel" or "":
-                    print("Cancelling settings change...")
+                # Check if the user wants to cancel
+                if api_key.lower() == "cancel" or api_key == "":
+                    self.console.print(
+                        Panel("[bold][red]Cancelling Settings Change...[/red][/bold]", title="Information",
+                              title_align='center', border_style='white'))
                     continue
 
+                # Otherwise, set the API key
                 self.set_openai_api_key(api_key)
-                print("\nAPI key set.")
+                self.console.print(
+                    Panel("[bold][green]API Key Set![/green][/bold]", title="Information", title_align='center',
+                          border_style='white'))
 
-            if setting == 'max_frames':
+            elif setting == 'max_frames':
+                max_frames = self.console.input("[green]Enter the max number of frames (1-10):[/green]")
 
-                max_frames = int(input("\nEnter the max number of frames (1-10): "))
-
-                if max_frames > 10 or max_frames < 1:
-                    print("Error. Frame value out of range.")
-                elif str(max_frames).lower() == "cancel" or "":
-                    print("\nCancelling settings change...")
+                # Check if the user wants to cancel
+                if max_frames.lower() == "cancel" or max_frames == "":
+                    self.console.print(
+                        Panel("[bold][red]Cancelling settings change...[/red][/bold]", title="Information",
+                              title_align='center', border_style='white'))
                     continue
 
-                self.set_max_frames(max_frames)
-                print("\nMax frames set.")
+                # Validate and set max_frames
+                try:
+                    max_frames = int(max_frames)
+                    if max_frames > 10 or max_frames < 1:
+                        self.console.print(
+                            Panel("[bold][red]Error. Frame value out of range.[/red][/bold]", title="Error",
+                                  title_align='center', border_style='white'))
+                        continue
+                    self.set_max_frames(max_frames)
+                    self.console.print(
+                        Panel("[bold][green]Max frames set.[/green][/bold]", title="Information", title_align='center',
+                              border_style='white'))
+                except ValueError:
+                    self.console.print(
+                        Panel("[bold][red]Please enter a valid integer or type 'cancel'.[/red][/bold]", title="Error",
+                              title_align='center', border_style='white'))
+                    continue
 
-            if setting.lower() == 'exit':
-                print("\nExiting Settings...")
+            elif setting.lower() == 'exit':
+                self.console.print(
+                    Panel("[bold][red]Exiting Settings...[/red][/bold]", title="Information", title_align='center',
+                          border_style='white'))
                 break
 
-            if setting not in ["openai_api_key", "max_frames", "exit"]:
-                print("\nInvalid input.")
-                continue
+            elif setting not in ["openai_api_key", "max_frames", "exit"]:
+                self.console.print(Panel("[bold][red]Invalid input.[/red][/bold]", title="Error", title_align='center',
+                                         border_style='white'))
 
     def load_settings(self):
         if os.path.exists(self.settings_file_path):
@@ -72,3 +104,7 @@ class Settings:
 
     def get_max_frames(self):
         return self.current_settings.get('max_frames', None)
+
+    def check_for_api_key(self) -> bool:
+        if self.current_settings['openai_api_key'] is not None:
+            return True
